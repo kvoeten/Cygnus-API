bad61f722b4232707662e147c6a3fda70afde648
Kaz Voeten <kaz_voeten@live.nl>
2019-03-10 01:03:18 +0100

    Fixed improper login messages and handling of accounts.

--- app/Http/Controllers/JoinController.php
+++ app/Http/Controllers/JoinController.php
@@ -20,10 +20,12 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use DateTime;
 use Carbon\Carbon;
 use App\Activation;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Hash;
 
@@ -52,35 +54,45 @@ class JoinController extends Controller {
 		$body = json_decode((string)$response->getBody());
 
 		if (!$body->success) {
-			return $this->error("Improper Captcha Response.", 400);
+			return $this->error("Improper Captcha Response.", 200);
 		}
-
+		
 		if (strlen($request->get('name')) < 3) {
-			return $this->error("The provided name is too short. (At least three characters required.)", 400);
+			return $this->error("The provided name is too short. (At least three characters required.)", 200);
 		}
 
 		if (strlen($request->get('name')) > 40) {
-			return $this->error("The provided name is too long. (At max. fourty characters are allowed.)", 400);
+			return $this->error("The provided name is too long. (At max. fourty characters are allowed.)", 200);
 		}
 
 		if (User::where('email', '=', $email)->exists()) {
-		    return $this->error("The provided email adress is already registered.", 400);
+		    return $this->error("The provided email adress is already registered.", 200);
 		}
 
 		if (User::where('name', '=', $request->get('name'))->exists()) {
-		    return $this->error("That name is already taken.", 400);
+		    return $this->error("That name is already taken.", 200);
 		}
 
 		if ($password != $request->get('password_confirmation')) {
-			return $this->error("The provided passwords don't match.", 400);
+			return $this->error("The provided passwords don't match.", 200);
 		}
 		
-		if (!checkdate($request->get('month')), $request->get('day')), $request->get('year')))) {
-			return $this->error("Invalid birthday.", 400);
+		$birthday = $request->get('birthday');
+		if (!$this->validateDate($birthday, 'Y-m-d')) {
+			return $this->error("Invalid birthday.", 200);
 		}
 		
-		if (request->get('gender') < 0 || request->get('gender') > 2) {
-			return $this->error("Invalid gender.", 400);
+		if ($request->get('gender') != "male" 
+			&& $request->get('gender') != "female" 
+			&& $request->get('gender') != "undefined") {
+			return $this->error("Invalid gender.", 200);
+		}
+		
+		$gender = 2;	
+		if ($request->get('gender') == "male") {
+			$gender = 0;
+		} else if ($request->get('gender') == "female"){
+			$gender = 1;
 		}
 
 		$user = User::create([
@@ -95,9 +107,8 @@ class JoinController extends Controller {
 			'activation_key' => $token
 		]);
 		
-		$birthdate = "".$request->get('year')."-".$request->get('month'))."-".$request->get('day'))." 00:00:00";
 		DB::insert('insert into accounts (nAccountID, nGender, pBirthDate) values (?, ?, ?)', 
-			[$user->id, request->get('gender'), $birthdate]);
+			[$user->id, $gender, $birthday]);
 
 		Mail::send(
 			'emails.email', 
@@ -119,6 +130,11 @@ class JoinController extends Controller {
 			return $this->error("An unknown error occured.", 422);
 		}
 	}
+	
+	function validateDate($date, $format = 'Y-m-d H:i:s'){
+		$d = DateTime::createFromFormat($format, $date);
+		return $d && $d->format($format) == $date;
+	}
 
 	public function validateRequest(Request $request) {
 		$rules = [
@@ -126,9 +142,7 @@ class JoinController extends Controller {
 			'email' => 'required',
 			'password' => 'required',
 			'password_confirmation' => 'required',
-			'month' => 'required',
-			'day' => 'required',
-			'year' => 'required',
+			'birthday' => 'required',
 			'gender' => 'required',
 			'g-recaptcha-response' => 'required'
 		];
